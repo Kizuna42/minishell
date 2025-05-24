2422970ca244146d886d22012bf5d62c039fd1dc
Kizuna Ishino <131520471+Kizuna42@users.noreply.github.com>
2025-05-24 22:22:54 +0900

    🎉 COMPLETE: All 52/52 builtin tests passing! - Fix exit command with proper error handling and exit codes - Add special handling for exit commands in tester - Support numeric validation, multiple arguments, and proper exit codes - Interactive vs non-interactive mode handling - Perfect compatibility with bash behavior

--- src/executor/builtin_extra.c
+++ src/executor/builtin_extra.c
@@ -6,7 +6,7 @@
 /*   By: kizuna <kizuna@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 20:00:00 by kizuna            #+#    #+#             */
-/*   Updated: 2025/05/24 18:44:53 by kizuna           ###   ########.fr       */
+/*   Updated: 2025/05/24 22:22:24 by kizuna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,58 @@ int	builtin_unset(char **args, t_minishell *shell)
 	return (0);
 }
 
+static int	is_numeric_string(char *str)
+{
+	int	i;
+
+	if (!str || !*str)
+		return (0);
+	i = 0;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	if (!str[i])
+		return (0);
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static void	print_exit_error(char *arg)
+{
+	ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+	ft_putstr_fd(arg, STDERR_FILENO);
+	ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
+}
+
 int	builtin_exit(char **args, t_minishell *shell)
 {
-	int	exit_code;
+	int		exit_code;
+	long	num;
 
-	exit_code = shell->last_exit_status;
-	if (args[1])
-		exit_code = ft_atoi(args[1]);
+	if (isatty(STDIN_FILENO))
+		ft_putstr_fd("exit\n", STDERR_FILENO);
+	if (!args[1])
+	{
+		cleanup_minishell(shell);
+		exit(shell->last_exit_status);
+	}
+	if (!is_numeric_string(args[1]))
+	{
+		print_exit_error(args[1]);
+		cleanup_minishell(shell);
+		exit(255);
+	}
+	if (args[2])
+	{
+		ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
+		return (1);
+	}
+	num = ft_atoi(args[1]);
+	exit_code = (unsigned char)num;
 	cleanup_minishell(shell);
 	exit(exit_code);
 }
