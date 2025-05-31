c74d2c174403677cfc84f42e88b3e088738127a3
Kizuna Ishino <131520471+Kizuna42@users.noreply.github.com>
2025-06-01 02:05:17 +0900

    Fix: improve cd and env builtin error handling to match bash behavior

--- src/utils/error_utils.c
+++ src/utils/error_utils.c
@@ -6,7 +6,7 @@
 /*   By: kizuna <kizuna@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 20:00:00 by kizuna            #+#    #+#             */
-/*   Updated: 2025/05/30 19:13:44 by kizuna           ###   ########.fr       */
+/*   Updated: 2025/06/01 02:03:09 by kizuna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,14 @@ int	handle_permission_error(char *command)
 {
 	print_error(command, "Permission denied");
 	return (126);
+}
+
+int	handle_cd_error(char *path)
+{
+	char	*error_msg;
+
+	error_msg = ft_strjoin(path, ": No such file or directory");
+	print_error("cd", error_msg);
+	free(error_msg);
+	return (1);
 }
