17278052d74c9761d7f09dac57bc87bb974a7c62
Kizuna Ishino <131520471+Kizuna42@users.noreply.github.com>
2025-06-11 17:20:14 +0900

    feat: implement signal handling for minishell

--- src/utils/signal_utils.c
+++ src/utils/signal_utils.c
@@ -6,7 +6,7 @@
 /*   By: kizuna <kizuna@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 20:00:00 by kizuna            #+#    #+#             */
-/*   Updated: 2025/06/11 17:12:36 by kizuna           ###   ########.fr       */
+/*   Updated: 2025/06/11 17:19:52 by kizuna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,19 +27,36 @@ static void	handle_sigint(int sig)
 static void	handle_sigquit(int sig)
 {
 	(void)sig;
+	return ;
+}
+
+static void	disable_quit_char(void)
+{
+	struct termios	term;
+
+	if (isatty(STDIN_FILENO) && tcgetattr(STDIN_FILENO, &term) == 0)
+	{
+		term.c_cc[VQUIT] = 0;
+		term.c_lflag &= ~ISIG;
+		tcsetattr(STDIN_FILENO, TCSANOW, &term);
+		term.c_lflag |= ISIG;
+		term.c_cc[VQUIT] = _POSIX_VDISABLE;
+		tcsetattr(STDIN_FILENO, TCSANOW, &term);
+	}
 }
 
 void	setup_signal_handlers(void)
 {
-	struct sigaction	sa_int;
-	struct sigaction	sa_quit;
+	struct sigaction	sa;
 
-	sa_int.sa_handler = handle_sigint;
-	sigemptyset(&sa_int.sa_mask);
-	sa_int.sa_flags = SA_RESTART;
-	sigaction(SIGINT, &sa_int, NULL);
-	sa_quit.sa_handler = handle_sigquit;
-	sigemptyset(&sa_quit.sa_mask);
-	sa_quit.sa_flags = SA_RESTART;
-	sigaction(SIGQUIT, &sa_quit, NULL);
+	sigemptyset(&sa.sa_mask);
+	sigaddset(&sa.sa_mask, SIGQUIT);
+	sa.sa_handler = handle_sigint;
+	sa.sa_flags = SA_RESTART;
+	sigaction(SIGINT, &sa, NULL);
+	sigemptyset(&sa.sa_mask);
+	sa.sa_handler = handle_sigquit;
+	sa.sa_flags = SA_RESTART;
+	sigaction(SIGQUIT, &sa, NULL);
+	disable_quit_char();
 }
